e418a6000e3205b27e794fc9bef7f422397003cd
villarley <santivillarley1010@gmail.com>
2025-02-22 00:21:44 -0600

    feat: added decorators from "class-validator"

--- src/resolvers/dtos/createCampaign.dto.ts
+++ src/resolvers/dtos/createCampaign.dto.ts
@@ -1,36 +1,48 @@
 import { InputType, Field, Int } from '@nestjs/graphql';
-import GraphQLJSON from 'graphql-type-json';
-import { InputJsonObject } from '@prisma/client/runtime/library';
+import { IsString, IsNotEmpty, IsInt, IsOptional } from 'class-validator';
 
 @InputType()
 export class CampaignCreateInput {
   @Field(() => Int)
+  @IsInt()
   campaign_id: number;
 
   @Field(() => String)
+  @IsNotEmpty()
+  @IsString()
   campaign_name: string;
 
   @Field(() => String)
+  @IsNotEmpty()
+  @IsString()
   campaign_description: string;
 
   @Field(() => String)
+  @IsNotEmpty()
+  @IsString()
   cover_photo: string;
 
-  @Field(() => GraphQLJSON, { nullable: true })
-  social_links?: InputJsonObject;
-
   @Field(() => Int)
+  @IsInt()
   target_amount: number;
 
   @Field(() => Int, { nullable: true })
+  @IsOptional()
+  @IsInt()
   totalDonations?: number;
 
   @Field(() => String)
+  @IsNotEmpty()
+  @IsString()
   organizer: string;
 
   @Field(() => String)
+  @IsNotEmpty()
+  @IsString()
   beneficiary: string;
 
   @Field(() => Int, { nullable: true })
+  @IsOptional()
+  @IsInt()
   category_id?: number;
 }
