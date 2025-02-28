a8a8409d1f654116b0963c42b491faab3d987aaa
villarley <santivillarley1010@gmail.com>
2025-02-28 00:44:52 -0600

    fix: requested changes

--- src/resolvers/dtos/createCampaign.dto.ts
+++ src/resolvers/dtos/createCampaign.dto.ts
@@ -1,5 +1,7 @@
 import { InputType, Field, Int } from '@nestjs/graphql';
 import { IsString, IsNotEmpty, IsInt, IsOptional } from 'class-validator';
+import GraphQLJSON from 'graphql-type-json';
+import { InputJsonObject } from '@prisma/client/runtime/library';
 
 @InputType()
 export class CampaignCreateInput {
@@ -45,4 +47,6 @@ export class CampaignCreateInput {
   @IsOptional()
   @IsInt()
   category_id?: number;
+  @Field(() => GraphQLJSON, { nullable: true })
+  social_links?: InputJsonObject;
 }
