28ef62c6045b5580b86a582a21d742e934b5bf82
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-03-28 09:01:48 +0100

    ADD: swagger config file

---
+++ src/swagger.config.ts
@@ -0,0 +1,8 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const swaggerConfig = new DocumentBuilder()
+  .setTitle('My API')
+  .setDescription('API documentation with detailed descriptions, request/response examples, and error handling')
+  .setVersion('1.0')
+  .addTag('Users') // Add relevant tags
+  .build();
